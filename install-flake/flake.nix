c6b762c6c84242226d2de64c42b5676f52d29bca
Max Tyler <maxastyler@gmail.com>
2022-02-22 09:49:47 +0000

    Update stuff

--- install-flake/flake.nix
+++ install-flake/flake.nix
@@ -1,14 +1,30 @@
 {
-  description = "Use this flake to generate a configuration based on the README.org file";
+  description =
+    "Use this flake to generate a configuration based on the README.org file";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
     system-source = {
       url = "../";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
     };
   };
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system: {
-    });
+  outputs = { self, nixpkgs, home-manager, emacs-overlay, system-source, ... }@attrs: {
+    nixosConfigurations.cheeky-monkey = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = attrs;
+      modules = [
+        ("${system-source}/src/configuration.nix")
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.max =
+            import ("${system-source}/src/home-manager.nix");
+        }
+      ];
+    };
+  };
 }
